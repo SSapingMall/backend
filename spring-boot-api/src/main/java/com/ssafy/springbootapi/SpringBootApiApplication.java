4e8b01d6d4bf0a8d0c360f4d7e210bba2ff579f8
kkh <kkho9654@naver.com>
2024-03-27 12:22:20 +0000

    feat:spring-boot #18

---
+++ spring-boot-api/src/main/java/com/ssafy/springbootapi/SpringBootApiApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.springbootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiApplication.class, args);
+	}
+
+}
---
+++ spring-boot-api/src/test/java/com/ssafy/springbootapi/SpringBootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.springbootapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
