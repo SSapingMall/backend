d14ebc0b5b03029970a409acc5881a91a03af507
kkh <kkho9654@naver.com>
2024-03-29 21:00:11 +0900

    feat:devcontainer #18

--- start_mysql.sh
+++ start_mysql.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # MySQL 서비스 시작
-service mysql start
+#service mysql start
+#!/bin/bash
+
+# MySQL 서비스 시작
+/etc/init.d/mysql start
+
+# 컨테이너가 계속 실행 상태로 유지되도록 하는 명령어
+tail -f /dev/null
 
 # 초기 설정 실행
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS mydb;"
