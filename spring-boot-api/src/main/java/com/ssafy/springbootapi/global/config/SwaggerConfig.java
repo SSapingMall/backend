1300a86af56c84c7724860fad37eeb7e88a03049
wintiger98 <inyub98@gmail.com>
2024-04-02 12:35:19 +0000

    fix : table init not working move : config -> global

--- spring-boot-api/src/main/java/com/ssafy/springbootapi/global/config/SwaggerConfig.java
+++ spring-boot-api/src/main/java/com/ssafy/springbootapi/global/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.ssafy.springbootapi.config;
+package com.ssafy.springbootapi.global.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
