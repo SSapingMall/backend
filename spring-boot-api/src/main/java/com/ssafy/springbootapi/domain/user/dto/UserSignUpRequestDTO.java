d5c8c08c9a596a4a9e32bbfedf1cae385b70a8fe
kkh <kkho9654@naver.com>
2024-04-05 08:48:57 +0000

    refactor: dto `@NotBlank` annotation 추가

--- spring-boot-api/src/main/java/com/ssafy/springbootapi/domain/user/dto/UserSignUpRequestDTO.java
+++ spring-boot-api/src/main/java/com/ssafy/springbootapi/domain/user/dto/UserSignUpRequestDTO.java
@@ -1,15 +1,21 @@
 package com.ssafy.springbootapi.domain.user.dto;
 
 import com.ssafy.springbootapi.domain.user.domain.User;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
 public class UserSignUpRequestDTO {
-    private String email;
-    private String password;
-    private String name;
+    @NotBlank(message = "email is blank!")
+    public String email;
+
+    @NotBlank(message = "password is blank!")
+    public String password;
+
+    @NotBlank(message = "name is blank!")
+    public String name;
 
     public User toEntity(){
         return User.builder()
