29884d0f60db83d28593ea8054383e1d56007757
kkh <kkho9654@naver.com>
2024-04-05 08:50:36 +0000

    refactor: findByEmail() returns Optional instance in repository

--- spring-boot-api/src/main/java/com/ssafy/springbootapi/domain/user/dao/UserRepository.java
+++ spring-boot-api/src/main/java/com/ssafy/springbootapi/domain/user/dao/UserRepository.java
@@ -4,7 +4,9 @@ import com.ssafy.springbootapi.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
