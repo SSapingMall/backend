7cbded7811ec11e7bc326aba9ecf7e4074395028
wintiger98 <inyub98@gmail.com>
2024-04-06 03:07:32 +0000

    fix : 필요없는 커밋 삭제

--- spring-boot-api/src/main/java/com/ssafy/springbootapi/domain/product/dao/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.springbootapi.domain.product.dao;
-
-import com.ssafy.springbootapi.domain.product.domain.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
