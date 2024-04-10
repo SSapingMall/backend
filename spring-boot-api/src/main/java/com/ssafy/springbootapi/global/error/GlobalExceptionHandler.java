754fe553272e9265a3c7504765939f45a4c3c0db
chanmin97 <cmbear314@naver.com>
2024-04-10 15:27:38 +0000

    :sparkles: [Add] GlobalExceptionHandler 추가

---
+++ spring-boot-api/src/main/java/com/ssafy/springbootapi/global/error/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.ssafy.springbootapi.global.error;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 내부 오류가 발생했습니다.");
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFoundException(EntityNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("존재하지 않습니다.");
+    }
+
+
+
+}
