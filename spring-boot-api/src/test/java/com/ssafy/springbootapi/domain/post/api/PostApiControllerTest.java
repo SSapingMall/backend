f5f5f809dc5370722f6164755a39ade457bddca0
chanmin97 <cmbear314@naver.com>
2024-04-15 12:42:09 +0000

    :sparkles: Add: PostApiControllerTest

---
+++ spring-boot-api/src/test/java/com/ssafy/springbootapi/domain/post/api/PostApiControllerTest.java
@@ -0,0 +1,79 @@
+package com.ssafy.springbootapi.domain.post.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssafy.springbootapi.domain.post.dao.PostRepository;
+import com.ssafy.springbootapi.domain.post.domain.Post;
+import com.ssafy.springbootapi.domain.post.dto.AddPostRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PostApiControllerTest {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    PostRepository postRepository;
+
+    @BeforeEach
+    public void mockMvcSetUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .build();
+        postRepository.deleteAll();
+    }
+
+    @DisplayName("게시글 생성 성공 테스트")
+    @Test
+    public void 게시글생성성공테스트() throws Exception {
+        // given
+        final String url = "/api/v1/posts";
+        final String title = "title";
+        final String content = "content";
+        final AddPostRequest userRequest = new AddPostRequest(title, content);
+
+        final String requestBody = objectMapper.writeValueAsString(userRequest);
+
+        // when
+        ResultActions result = mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(requestBody));
+
+        // then
+        result.andExpect(status().isCreated());
+
+        List<Post> posts = postRepository.findAll();
+
+        assertThat(posts.size()).isEqualTo(1);
+        assertThat(posts.get(0).getTitle()).isEqualTo(title);
+        assertThat(posts.get(0).getContent()).isEqualTo(content);
+    }
+
+
+}
