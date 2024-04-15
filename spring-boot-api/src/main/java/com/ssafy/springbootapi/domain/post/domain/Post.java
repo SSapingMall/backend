5a97f4761326aa7d68b79273753908339f46044d
chanmin97 <cmbear314@naver.com>
2024-04-15 12:41:47 +0000

    :sparkles: Add: Post

--- spring-boot-api/src/main/java/com/ssafy/springbootapi/domain/post/domain/Post.java
+++ spring-boot-api/src/main/java/com/ssafy/springbootapi/domain/post/domain/Post.java
@@ -4,24 +4,27 @@ package com.ssafy.springbootapi.domain.post.domain;
 import com.ssafy.springbootapi.domain.user.domain.User;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
+@Builder
 @Entity
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", updatable = false)
     private Long id;
 
     @Column(name = "title", nullable = false)
     private String title;
 
-    @Column(name = "content", nullable = false)
+    @Column(name = "contents", nullable = false)
     private String content;
 
     @CreatedDate
@@ -32,24 +35,21 @@ public class Post {
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
-    @Column(name = "likes", nullable = false)
-    private Long likes;
+    @Builder.Default
+    @Column(name = "likes")
+    private Long likes=0L;
 
-    @Column(name = "dislikes", nullable = false)
-    private Long dislikes;
+    @Builder.Default
+    @Column(name = "dislikes")
+    private Long dislikes=0L;
 
-    @Column(name = "views", nullable = false)
-    private Long views;
+    @Builder.Default
+    @Column(name = "views")
+    private Long views=0L;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
-
-    @Builder
-    public Post(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
 
     public void update(String title, String content) {
         this.title = title;
