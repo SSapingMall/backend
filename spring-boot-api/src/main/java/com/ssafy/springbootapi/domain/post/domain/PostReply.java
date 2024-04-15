585be82279b830539de77f11beb976b38ff87b5a
chanmin97 <cmbear314@naver.com>
2024-04-15 10:42:51 +0000

    :recycle: Refactor: PostReply

--- spring-boot-api/src/main/java/com/ssafy/springbootapi/domain/post/domain/PostReply.java
+++ spring-boot-api/src/main/java/com/ssafy/springbootapi/domain/post/domain/PostReply.java
@@ -3,22 +3,31 @@ package com.ssafy.springbootapi.domain.post.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Getter @Setter
+@Getter
 public class PostReply {
     @Id
     @GeneratedValue
+    @Column(name = "id", updatable = false)
     private Long id;
 
-    private String contents;
+    @Column(name = "content", nullable = false)
+    private String content;
 
+    @CreatedDate
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
 
+    @LastModifiedDate
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
+    @Column(name = "likes", nullable = false)
     private Long likes;
 
     @ManyToOne(fetch = FetchType.LAZY)
