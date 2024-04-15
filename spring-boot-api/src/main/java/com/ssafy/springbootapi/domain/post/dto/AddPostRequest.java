d6efc0ff163c643d5e277222e0cb298bb9436c9c
chanmin97 <cmbear314@naver.com>
2024-04-15 10:46:46 +0000

    :sparkles: Add: AddPostRequest

--- spring-boot-api/src/main/java/com/ssafy/springbootapi/domain/post/dto/AddPostRequest.java
+++ spring-boot-api/src/main/java/com/ssafy/springbootapi/domain/post/dto/AddPostRequest.java
@@ -1,4 +1,23 @@
 package com.ssafy.springbootapi.domain.post.dto;
 
+import com.ssafy.springbootapi.domain.post.domain.Post;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class AddPostRequest {
+    private String title;
+
+    private String content;
+
+    public Post toEntity() {
+        return Post.builder()
+                .title(title)
+                .content(content)
+                .build();
+    }
 }
