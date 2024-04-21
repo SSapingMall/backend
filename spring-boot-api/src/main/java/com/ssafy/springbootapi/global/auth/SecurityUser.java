dcd55a7bf3b9fb1d2573fd576179cc8efeae357d
kkh <kkho9654@naver.com>
2024-04-21 10:39:11 +0000

    Feat: SecurityUser implements UserDetails

---
+++ spring-boot-api/src/main/java/com/ssafy/springbootapi/global/auth/SecurityUser.java
@@ -0,0 +1,57 @@
+package com.ssafy.springbootapi.global.auth;
+
+import com.ssafy.springbootapi.domain.user.domain.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class SecurityUser implements UserDetails {
+    private final User user;
+
+    public SecurityUser(User user) {
+        this.user = user;
+    }
+
+    // 권한 반환
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("user"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    // 계정 만료여부 반환
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    // 계정 잠금 여부 반환
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    // 패스워드 만료 여부 반환
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    // 계정 사용 가능 여부 반환
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
