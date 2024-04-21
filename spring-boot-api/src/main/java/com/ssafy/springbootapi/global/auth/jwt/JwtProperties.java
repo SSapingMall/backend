9bbadb0162762cbca0ca57611c5fabb0e9ca76b3
kkh <kkho9654@naver.com>
2024-04-21 10:40:48 +0000

    Feat: jwt token entity, 발급자, security key

---
+++ spring-boot-api/src/main/java/com/ssafy/springbootapi/global/auth/jwt/JwtProperties.java
@@ -0,0 +1,15 @@
+package com.ssafy.springbootapi.global.auth.jwt;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Setter
+@Getter
+@Component
+@ConfigurationProperties("jwt")
+public class JwtProperties {
+    private String issuer;
+    private String secretKey;
+}
