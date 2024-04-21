5c21b5cdd0f8af0dd9011c756595fd2d396201e9
kkh <kkho9654@naver.com>
2024-04-21 10:39:25 +0000

    Feat: UserDetailService

---
+++ spring-boot-api/src/main/java/com/ssafy/springbootapi/global/auth/UserDetailService.java
@@ -0,0 +1,24 @@
+package com.ssafy.springbootapi.global.auth;
+
+import com.ssafy.springbootapi.domain.user.dao.UserRepository;
+import com.ssafy.springbootapi.domain.user.domain.User;
+import com.ssafy.springbootapi.domain.user.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(()->new UsernameNotFoundException(email+" 사용자 없음"));
+        SecurityUser securityUser = new SecurityUser(user);
+        return securityUser;
+    }
+}
