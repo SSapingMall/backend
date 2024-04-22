a3dcd1c3189f6dff9596dd26453b92454fe84e9a
kkh <kkho9654@naver.com>
2024-04-22 08:32:53 +0000

    🐛Fix: user update

--- spring-boot-api/src/main/java/com/ssafy/springbootapi/domain/user/domain/UserMapper.java
+++ spring-boot-api/src/main/java/com/ssafy/springbootapi/domain/user/domain/UserMapper.java
@@ -6,8 +6,6 @@ import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
-import java.util.Optional;
-
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
