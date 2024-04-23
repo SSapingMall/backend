da6af3f79872e28f2376b9d07814ae5e6cf378e6
wintiger98 <inyub98@gmail.com>
2024-04-23 02:27:08 +0000

    :sparkles: GlobalHandler에 UserNotFound Exception 추가 : 상품에서도 해당 예외 처리 사용하기 때문

--- spring-boot-api/src/main/java/com/ssafy/springbootapi/global/error/GlobalExceptionHandler.java
+++ spring-boot-api/src/main/java/com/ssafy/springbootapi/global/error/GlobalExceptionHandler.java
@@ -1,7 +1,8 @@
 package com.ssafy.springbootapi.global.error;
 
  import com.ssafy.springbootapi.domain.product.exception.NotFoundProductException;
-import org.springframework.http.HttpStatus;
+ import com.ssafy.springbootapi.domain.user.exception.UserNotFoundException;
+ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,6 +12,13 @@ public class GlobalExceptionHandler { // Domain별 Exception 핸들러
 
      @ExceptionHandler(NotFoundProductException.class)
      public ResponseEntity<?> handleNotFoundProductException(NotFoundProductException ex) {
-         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
      }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ex.getMessage());
+    }
 }
