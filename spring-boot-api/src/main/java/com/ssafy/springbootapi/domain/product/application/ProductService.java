585a84548082cfff4d47cbbf73586ee8f2cc4323
wintiger98 <inyub98@gmail.com>
2024-04-23 02:27:35 +0000

    :sparkles: insert, update 안 되는 이슈 해결

--- spring-boot-api/src/main/java/com/ssafy/springbootapi/domain/product/application/ProductService.java
+++ spring-boot-api/src/main/java/com/ssafy/springbootapi/domain/product/application/ProductService.java
@@ -1,6 +1,7 @@
 package com.ssafy.springbootapi.domain.product.application;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import com.ssafy.springbootapi.domain.product.dao.ProductRepository;
@@ -11,14 +12,22 @@ import com.ssafy.springbootapi.domain.product.dto.ProductListOutput;
 import com.ssafy.springbootapi.domain.product.dto.ProductOutput;
 import com.ssafy.springbootapi.domain.product.dto.ProductUpdate;
 import com.ssafy.springbootapi.domain.product.exception.NotFoundProductException;
+import com.ssafy.springbootapi.domain.user.dao.UserRepository;
+import com.ssafy.springbootapi.domain.user.domain.User;
+import com.ssafy.springbootapi.domain.user.exception.UserNotFoundException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
+    private final UserRepository userRepository;
     private final ProductMapper productMapper;
+
 
     /**
      * Product 전체 조회
@@ -47,9 +56,22 @@ public class ProductService {
      * @return ProductOutput(DTO)
      */
     public ProductOutput insertProduct(ProductInput productInput) {
+        // user_id로 User 객체 찾기
+        Optional<User> userOptional = userRepository.findById(productInput.getUser_id());
+        User user = userOptional.orElseThrow(() ->
+                new UserNotFoundException("User not found with id: " + productInput.getUser_id())
+        );
+
+        // ProductInput에서 Product 엔티티로 변환
         Product product = productMapper.toEntity(productInput);
+
+        // User 객체를 Product 엔티티에 설정
+        product.setUser(user);
+
+        // Product 엔티티를 DB에 저장하고, 저장된 엔티티를 ProductOutput DTO로 변환하여 반환
         return productMapper.toProductOutput(productRepository.save(product));
     }
+
 
     /**
      * Product 삭제 메서드
@@ -68,6 +90,7 @@ public class ProductService {
      * @param productUpdate : 수정할 product 데이터
      * @return ProductOutput(DTO)
      */
+    @Transactional
     public ProductOutput updateProduct(Long id, ProductUpdate productUpdate) {
         Product productToUpdate = findProductByIdOrThrow(id);
         productMapper.updateProduct(productUpdate, productToUpdate);
