3d9c29561d8f3f544e2f4f771cdc2abed2895e08
kkh <kkho9654@naver.com>
2024-05-02 11:27:45 +0000

    :sparkles: Feat: swagger authentication

--- spring-boot-api/src/main/java/com/ssafy/springbootapi/global/config/SwaggerConfig.java
+++ spring-boot-api/src/main/java/com/ssafy/springbootapi/global/config/SwaggerConfig.java
@@ -2,6 +2,10 @@ package com.ssafy.springbootapi.global.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
@@ -23,5 +27,19 @@ public class SwaggerConfig {
                 .group("ssappingmall API v1")  // 그룹 이름을 설정한다.
                 .pathsToMatch(paths)     // 그룹에 속하는 경로 패턴을 지정한다.
                 .build();
+    }
+    @Bean
+    public OpenAPI api() {
+        SecurityScheme apiKey = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY)
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("Bearer Token");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("Bearer Token", apiKey))
+                .addSecurityItem(securityRequirement);
     }
 }
