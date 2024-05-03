5e4a39a8b82e051fd544358801e27a1f69964db2
kkh <kkho9654@naver.com>
2024-05-03 05:45:03 +0000

    :art: Fix: 유저 email update 불가능

--- spring-boot-api/src/main/java/com/ssafy/springbootapi/domain/user/dto/UserUpdateRequest.java
+++ spring-boot-api/src/main/java/com/ssafy/springbootapi/domain/user/dto/UserUpdateRequest.java
@@ -7,16 +7,6 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public class UserUpdateRequest {
-    public String email;
     public String password;
     public String name;
-
-    @Override
-    public String toString() {
-        return "UserSignUpRequestDTO{" +
-                "email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                ", name='" + name + '\'' +
-                '}';
-    }
 }
