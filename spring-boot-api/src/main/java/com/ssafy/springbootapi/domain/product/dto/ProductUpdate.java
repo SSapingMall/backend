7982abf0f8b1189ad0b0058cab9daa76bdc958a0
??? <inyub98@gmail.com>
2024-05-06 12:30:19 +0000

    :sparkles: Feat : category 속성 변경에 따른 Product DTO 변경

--- spring-boot-api/src/main/java/com/ssafy/springbootapi/domain/product/dto/ProductUpdate.java
+++ spring-boot-api/src/main/java/com/ssafy/springbootapi/domain/product/dto/ProductUpdate.java
@@ -3,7 +3,6 @@ package com.ssafy.springbootapi.domain.product.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @AllArgsConstructor
@@ -12,6 +11,6 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 public class ProductUpdate extends ProductBase {
     private String description;
-    private int category;
-    private int stock;
+    private Integer category_id;
+    private Integer stock;
 }
