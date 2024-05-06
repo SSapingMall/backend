7dfedb55b37f40839f1c8f66d1774c771991ef22
??? <inyub98@gmail.com>
2024-05-06 12:31:25 +0000

    :sparkles: Feat : Category Service내 CRUD 구현 완료 + Transactional 어노테이션 추가

--- spring-boot-api/src/main/java/com/ssafy/springbootapi/domain/product/application/CategoryService.java
+++ spring-boot-api/src/main/java/com/ssafy/springbootapi/domain/product/application/CategoryService.java
@@ -2,6 +2,8 @@ package com.ssafy.springbootapi.domain.product.application;
 
 import com.ssafy.springbootapi.domain.product.dao.CategoryRepository;
 import com.ssafy.springbootapi.domain.product.domain.Category;
+import com.ssafy.springbootapi.domain.product.exception.NotFoundCategoryException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -26,13 +28,58 @@ public class CategoryService {
      * @return Category
      */
     public Category getCategoryById(Integer id) {
-        return categoryRepository.getReferenceById(id);
+
+        return categoryRepository.findById(id).orElseThrow(()
+                -> new NotFoundCategoryException("Category not found with id : " + id));
     }
 
-    public Boolean createCategory(String name) {
-        Category category = new Category();
+    /**
+     * 카테고리 생성
+     * @param name : 카테고리 이름
+     * @return 생성된 카테고리
+     */
+    @Transactional
+    public Category createCategory(String name) {
+        try {
+            Category category = new Category();
+            category.setName(name);
+            return categoryRepository.save(category);
+        } catch(Exception E) {
+            return null;
+        }
+    }
+
+    /**
+     * 카테고리 이름 수정
+     * @param id : 수정할 카테고리 아이디
+     * @param name : 수정할 카테고리 이름
+     * @return 수정된 카테고리
+     */
+    @Transactional
+    public Category updateCategory(Integer id, String name) {
+        Category category = findCategoryByIdOrThrow(id);
         category.setName(name);
-        categoryRepository.save(category);
-        return true;
+        return category;
+    }
+
+    /**
+     * 카테고리 삭제
+     * @param id : 삭제할 카테고리 아이디
+     * @return 성공 여부
+     */
+    @Transactional
+    public Boolean deleteCategory(Integer id) {
+        Category category = findCategoryByIdOrThrow(id);
+        try {
+            categoryRepository.delete(category);
+            return true;
+        } catch(Exception e) {
+            return false;
+        }
+    }
+
+    private Category findCategoryByIdOrThrow(Integer id) {
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new NotFoundCategoryException("Category Not Found"));
     }
 }
