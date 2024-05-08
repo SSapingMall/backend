4639c4e4fd8a439bb188b62b94c5594ff404557c
kkh <kkho9654@naver.com>
2024-05-08 12:24:50 +0000

    :recycle: Refactor: UsernamePasswordAuthenticationFilter - setPasswordParameter 메소드 사용 - JsonProcessingAuthenticationException custom exception - 컨텐트 타입 체크 : equals->startWith - try-with-resources

---
+++ spring-boot-api/src/main/java/com/ssafy/springbootapi/global/error/JsonProcessingAuthenticationException.java
@@ -0,0 +1,9 @@
+package com.ssafy.springbootapi.global.error;
+
+import org.springframework.security.core.AuthenticationException;
+
+public class JsonProcessingAuthenticationException extends AuthenticationException {
+    public JsonProcessingAuthenticationException(String msg, Throwable t) {
+        super(msg, t);
+    }
+}
