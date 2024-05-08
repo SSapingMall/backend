09f3b80da86952aa0383204368bb8f4fc3809ad7
kkh <kkho9654@naver.com>
2024-05-08 12:36:49 +0000

    :art: Feat: 생성자

--- spring-boot-api/src/main/java/com/ssafy/springbootapi/global/error/JsonProcessingAuthenticationException.java
+++ spring-boot-api/src/main/java/com/ssafy/springbootapi/global/error/JsonProcessingAuthenticationException.java
@@ -3,6 +3,10 @@ package com.ssafy.springbootapi.global.error;
 import org.springframework.security.core.AuthenticationException;
 
 public class JsonProcessingAuthenticationException extends AuthenticationException {
+    public JsonProcessingAuthenticationException(String msg) {
+        super(msg);
+    }
+
     public JsonProcessingAuthenticationException(String msg, Throwable t) {
         super(msg, t);
     }
